a7160098a543b56b710583a1296368d1b883efc5
naoya <n.hasepb0214@outlook.jp>
2026-02-25 19:40:32 +0900

    remove mysql data from git tracking

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
